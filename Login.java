c937e89d4d7fc21c29889933bf3e008e9d8fe965
amit kumar <76870053+Amit2508@users.noreply.github.com>
2023-06-28 19:13:33 +0530

    Update Login.java
    Added Buttons : sign ,clear and signup

--- Login.java
+++ Login.java
@@ -1,26 +1,106 @@
 
 package bank.management.system;
-import javax.swing.*;
+import javax.swing.*;   //swing is used for creating frame
+import java.awt.*;  // images are in awt package 
+import java.awt.event.*;
 /**
  *
  * @author amitgoel
  */
-public class Login extends JFrame{
+public class Login extends JFrame implements ActionListener{
+    
+    JButton login ,signup,clear;
+    JTextField cardTextField;
+    JPasswordField pinTextField;
     Login()
             
     {
         setTitle("ATM");
         
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg"));
         
-        JLabel label = new JLabel(i1);
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg"));
+        Image i2=i1.getImage().getScaledInstance(100,100,Image.SCALE_DEFAULT);
+        ImageIcon i3=new ImageIcon(i2);
+        JLabel label = new JLabel(i3);  // we can take imageicon as arguement in jlabel but not image
+        label.setBounds(70,10,100,100);
         add(label);
+        
+        JLabel text= new JLabel("Welcome to ATM");  //jabel is majorly used for text label
+        text.setFont(new Font("Osward" , Font.BOLD,38)); 
+        text.setBounds(200,40,400,40);
+        add(text);
+        
+         JLabel cardno= new JLabel("Card No: ");  //jabel is majorly used for text label
+        cardno.setFont(new Font("Osward" , Font.BOLD,28)); 
+        cardno.setBounds(120, 150,150,30);
+        add(cardno);
+        
+        cardTextField= new JTextField();
+        cardTextField.setBounds(300,150,230,30);
+        add(cardTextField);
+        
+          JLabel pin= new JLabel("PIN: ");  //jabel is majorly used for text label
+        pin.setFont(new Font("Osward" , Font.BOLD,28)); 
+       pin.setBounds(120, 220,250,30);
+        add(pin);
+       pinTextField= new JPasswordField();
+        pinTextField.setBounds(300,220,230,30);
+        add(pinTextField);
+        
+        login = new JButton("SIGN IN");
+        login.setBounds(300,300,100,30);
+        
+        login.addActionListener(this);
+       
+               
+        add(login);
+        
+         clear = new JButton("CLEAR");
+        clear.setBounds(430,300,100,30);
+//        clear.setBackground(Color.BLACK);
+//       clear.setForeground(Color.WHITE);
+        clear.addActionListener(this);
+
+        add(clear);
+//        
+         signup = new JButton("SIGN UP");
+       signup.setBounds(300,350,230,30);
+//       signup.setBackground(Color.BLACK);
+//       signup.setForeground(Color.WHITE);
+        signup.addActionListener(this);
+
+        add(signup);
+        setLayout(null);  
+         
+        
+        getContentPane().setBackground(Color.WHITE); // changing frame background color
+                
         setSize(800 , 480);
         setVisible(true);
         setLocation(350,200);
+        
+    }
+    public void actionPerformed(ActionEvent ae)
+    {
+       
+        if(ae.getSource()==clear)
+        {
+            cardTextField.setText("");
+             pinTextField.setText("");
+
+        }
+        else if (ae.getSource()==login)
+        {
+            
+        }
+                else if (ae.getSource()==signup)
+        {
+            
+        }
     }
     
     public static void main(String[] args){
         new Login();
     }
 }
+ 
