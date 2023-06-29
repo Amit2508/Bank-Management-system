d60e014be191837cf1b4649480ff8afd8e8a63ef
amit kumar <76870053+Amit2508@users.noreply.github.com>
2023-06-29 13:42:18 +0530

    Update Conn.java
    Established JDBC connection

--- Conn.java
+++ Conn.java
@@ -1,1 +1,27 @@
 
+
+package bank.management.system;
+import java.sql.*;  
+
+/**
+ *
+ * @author amitgoel
+ */
+public class Conn {
+   Connection c;
+    Statement s;
+    
+    public Conn()
+    {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            c =DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","Amit@2508");    // creating connection , driver manager is class
+            s =c.createStatement();  // create statement
+
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+         
+    }
+}
